b85c243775cca47379a4ac007d67205e7b6dcd17
Pablo Gomez <pablonahuelgomez@gmail.com>
2018-02-27 17:38:32 -0300

    Refactor code, moving parts
    Introducing two new interchangeable objects: ErrorBuilder & ResponseHandler.
    The first creates SDK error responses and the second handles Expo's responses.

--- lib/exponent-server-sdk.rb
+++ lib/exponent-server-sdk.rb
@@ -10,53 +10,19 @@ module Exponent
   module Push
 
     class Client
-
-      def initialize(new_http_client = nil)
-        @http_client = new_http_client || Typhoeus
+      def initialize(http_client      = Typhoeus,
+                     response_handler = ResponseHandler.new)
+        @http_client      = http_client
+        @response_handler = response_handler
       end
 
       def publish(messages)
-        handle_response(push_notifications(messages))
+        response_handler.handle(push_notifications(messages))
       end
 
       private
 
-      attr_reader :http_client
-
-      def handle_response(response)
-        case response.code.to_s
-        when /(^4|^5)/
-          raise build_error_from_failure(parse_json(response))
-        else
-          handle_success(parse_json(response))
-        end
-      end
-
-      def parse_json(response)
-        JSON.parse(response.body)
-      end
-
-      def build_error_from_failure(response)
-        build_error_with_handling(response) do
-          extract_error_from_response(response)
-        end
-      end
-
-      def extract_error_from_response(response)
-        error = response.fetch('errors').first
-        error_name = error.fetch('code')
-        message = error.fetch('message')
-
-        validate_error_name(Exponent::Push.error_names.include?(error_name)) do
-          Exponent::Push.const_get("#{error_name}Error")
-        end.new(message)
-      end
-
-      def build_error_with_handling(response)
-        yield(response)
-      rescue KeyError
-        unknown_error_format(response)
-      end
+      attr_reader :http_client, :response_handler
 
       def push_notifications(messages)
         http_client.post(
@@ -76,6 +42,33 @@ module Exponent
           'Accept'       => 'application/json'
         }
       end
+    end
+
+    class ResponseHandler
+      def initialize(error_builder = ErrorBuilder.new)
+        @error_builder = error_builder
+      end
+
+      def handle(response)
+        case response.code.to_s
+        when /(^4|^5)/
+          raise build_error_from_failure(parse_json(response))
+        else
+          handle_success(parse_json(response))
+        end
+      end
+
+      private
+
+      attr_reader :error_builder
+
+      def parse_json(response)
+        JSON.parse(response.body)
+      end
+
+      def build_error_from_failure(response)
+        error_builder.build_from_erroneous(response)
+      end
 
       def handle_success(response)
         extract_data(response).tap do |data|
@@ -83,35 +76,59 @@ module Exponent
         end
       end
 
+      def extract_data(response)
+        response.fetch('data').first
+      end
+
       def validate_status(status, response)
         raise build_error_from_success(response) unless status == 'ok'
       end
 
       def build_error_from_success(response)
-        build_error_with_handling(response) do
-          extract_error_from_success(response)
+        error_builder.build_from_successful(response)
+      end
+    end
+
+    class ErrorBuilder
+      %i[erroneous successful].each do |selector|
+        define_method(:"build_from_#{selector}") do |response|
+          with_error_handling(response) do
+            send "from_#{selector}_response", response
+          end
         end
       end
 
-      def extract_error_from_success(response)
-        data    = extract_data(response)
+      private
+
+      def with_error_handling(response)
+        yield(response)
+      rescue KeyError
+        unknown_error_format(response)
+      end
+
+      def from_erroneous_response(response)
+        error      = response.fetch('errors').first
+        error_name = error.fetch('code')
+        message    = error.fetch('message')
+
+        get_error_class(error_name).new(message)
+      end
+
+      def from_successful_response(response)
+        data    = response.fetch('data').first
         message = data.fetch('message')
 
         get_error_class(data.fetch('details').fetch('error')).new(message)
-      end
-
-      def get_error_class(error_name)
-        validate_error_name(Exponent::Push.error_names.include?(error_name)) do
-          Exponent::Push.const_get("#{error_name}Error")
-        end
       end
 
       def validate_error_name(condition)
         condition ? yield : Exponent::Push::UnknownError
       end
 
-      def extract_data(response)
-        response.fetch('data').first
+      def get_error_class(error_name)
+        validate_error_name(Exponent::Push.error_names.include?(error_name)) do
+          Exponent::Push.const_get("#{error_name}Error")
+        end
       end
 
       def unknown_error_format(response)
